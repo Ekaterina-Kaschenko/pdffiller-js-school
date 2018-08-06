cd9f81982100090d834a664830dd882a82c31539
Kateryna <katikate123@gmail.com>
2018-08-06 18:21:42 +0300

    refactoring; ready to rewiev

--- lessons/lesson_2/task_1/scripts/index.js
+++ lessons/lesson_2/task_1/scripts/index.js
@@ -1,20 +1,39 @@
 var button = document.getElementsByClassName('button')[0];
 var table = document.getElementsByClassName('table')[0];
-var userName = document.getElementsByClassName('name')[0];
-var userBirthday = document.getElementsByClassName('birthday')[0];
-var userCity = document.getElementsByClassName('city')[0];
 
+function createTableRow(user) {
+  var tableRow = document.createElement('tr')
 
-button.addEventListener('click', function() {
-  var user = {};
-
-  user.fullName = prompt('Как вас зовут?', 'ФИО');
-  user.birthday = prompt('Ваша дата рождения?', '20/10/1992');
-  user.city = prompt('Где вы живете?', 'Город');
-
+  var userName = document.createElement('td');
+  userName.className = 'name';
   userName.textContent = user.fullName;
+
+  var userBirthday = document.createElement('td');
+  userBirthday.className = 'birthday';
   userBirthday.textContent = user.birthday;
+
+  var userCity = document.createElement('td');
+  userCity.className = 'city';
   userCity.textContent = user.city;
 
+  tableRow.appendChild(userName)
+  tableRow.appendChild(userBirthday)
+  tableRow.appendChild(userCity)
+
+  table.appendChild(tableRow)
+}
+
+button.addEventListener('click', function() {
   table.classList.remove('hidden');
+  var fullName = prompt('Как вас зовут?', 'ФИО');
+  var birthday = prompt('Ваша дата рождения?', '20/10/1992');
+  var city = prompt('Где вы живете?', 'Город');
+
+  var user = {
+    fullName,
+    birthday,
+    city
+  }
+
+  createTableRow(user);
 })
